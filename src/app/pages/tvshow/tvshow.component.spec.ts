f429c6e02130f221656d5882ac3770b37ec7636f
Femi Sowemimo <femi.sowems@gmail.com>
2023-11-21 08:36:50 -0500

    fix: fixed testing bug

--- src/app/pages/tvshow/tvshow.component.spec.ts
+++ src/app/pages/tvshow/tvshow.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MovieComponent } from '../movie/movie.component';
 
-import { MovieComponent } from './movie.component';
 
 describe('MovieComponent', () => {
   let component: MovieComponent;
